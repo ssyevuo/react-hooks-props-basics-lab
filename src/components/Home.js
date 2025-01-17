19a552b72d3d815865dedb8078d48d898772eb64
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-01-17 23:51:14 +0300

    Completed lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import user from "../data/user";
+console.log(user);
 
 function Home(props) {
   return (
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+import user from "../data/user.js"
+console.log(user);
+
+function Links({ github, linkedin}) {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={github}>{github}</a>
+            <a href={linkedin}>{linkedin}</a>
+        </div>
+    );
+}
+
+export default Links;
